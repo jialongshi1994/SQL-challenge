bdc42e120edf6c3cbeefbb103d796342f6686c42
jialongshi1994 <55209538+jialongshi1994@users.noreply.github.com>
2020-07-27 11:27:10 -0400

    Add files via upload

---
+++ SQL-challenge/data/1.employee_employee_departments.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d001', 'Marketing');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d002', 'Finance');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d003', 'Human Resources');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d004', 'Production');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d005', 'Development');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d006', 'Quality Management');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d007', 'Sales');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d008', 'Research');
+INSERT INTO employee.departments (dept_no, dept_name) VALUES ('d009', 'Customer Service');
---
+++ SQL-challenge/data/2.employee_employee_titles.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee.titles (title_id, title) VALUES ('s0001', 'Staff');
+INSERT INTO employee.titles (title_id, title) VALUES ('s0002', 'Senior Staff');
+INSERT INTO employee.titles (title_id, title) VALUES ('e0001', 'Assistant Engineer');
+INSERT INTO employee.titles (title_id, title) VALUES ('e0002', 'Engineer');
+INSERT INTO employee.titles (title_id, title) VALUES ('e0003', 'Senior Engineer');
+INSERT INTO employee.titles (title_id, title) VALUES ('e0004', 'Technique Leader');
+INSERT INTO employee.titles (title_id, title) VALUES ('m0001', 'Manager');
